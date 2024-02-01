5b129592557293112a6ab4cf22586dfdb8dc5f6a
Arman Piliposyan <arman_piliposyan@mail.ru>
2024-02-01 23:35:15 +0400

    create react app

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import { createRoot } from 'react-dom/client';
+import React from 'react';
+
+import App from './components/App';
+import './index.scss';
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement!);
+
+root.render(
+  // <React.StrictMode>
+  <App />,
+  // </React.StrictMode>,
+);
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires, no-undef
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const PLUGINS = [
+  new HtmlWebpackPlugin({
+    template: 'src/index.html',
+  }),
+];
+
+const RULES = [
+  {
+    use: {
+      loader: 'babel-loader',
+    },
+    exclude: /node_modules/,
+    test: /\.(ts|tsx)$/,
+  },
+  {
+    use: ['style-loader', 'css-loader', 'sass-loader'],
+    test: /\.(sa|sc|c)ss$/,
+  },
+];
+
+const EXTENSIONS = ['.tsx', '.ts', '.jsx', '.js', '.json'];
+
+// eslint-disable-next-line no-undef
+module.exports = {
+  resolve: {
+    extensions: EXTENSIONS,
+  },
+  devServer: {
+    port: 3000,
+  },
+  module: {
+    rules: RULES,
+  },
+  entry: './src/index.tsx',
+  plugins: PLUGINS,
+};
